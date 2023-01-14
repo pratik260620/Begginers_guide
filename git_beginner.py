5ac2ab0d612fb950caa1a4d8c5352eeb02fedec3
Pratik Khandelwal <pratikkhandelwal8484@gmail.com>
2023-01-14 22:09:44 +0530

    commit1

---
+++ git_beginner.py
@@ -0,0 +1,15 @@
+class Employee:
+    def __init__(self,fname,lname,sal):
+        self.firstname= fname
+        self.lastname= lname
+        self.salary=sal
+    def myfunc(self):
+        print("My name is {} and My surname is {}".format (self.firstname, self.lastname))
+class Person(Employee):
+    def __init__(self,fname,lname,sal,year):
+        super().__init__(fname,lname,sal)
+        self.gradyear= year
+
+p1= Person("Pratik", "Khandelwal", 50000, 2012)
+print (p1.gradyear)
+p1.myfunc()
